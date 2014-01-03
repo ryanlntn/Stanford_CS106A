7e1579340324f224279066a6dab25240f483e171
Ryan Linton <ryanlntn@gmail.com>
2014-01-02 22:49:56 -0800

    Get Karel working

---
+++ src/OurKarel.java
@@ -0,0 +1,17 @@
+/*
+ * File: OurKarel.java
+ * -----------------------------
+ * The OurKarel class extends the basic Karel class
+ * by defining a "run" method with three commands.  These
+ * commands cause Karel to move forward three blocks.
+ */
+
+import stanford.karel.*;
+
+public class OurKarel extends Karel {
+    public void run() {
+        move();
+        move();
+        move();
+    }
+}
