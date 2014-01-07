770f7d96343886759d067f1dbe4c3a13789540fe
Ryan Linton <ryanlntn@gmail.com>
2014-01-07 09:52:27 -0800

    Solve Problem 4 for all but 1 corner wide edge case

--- src/Assignment1/MidpointFindingKarel.java
+++ src/Assignment1/MidpointFindingKarel.java
@@ -14,6 +14,41 @@ import stanford.karel.*;
 
 public class MidpointFindingKarel extends SuperKarel {
 
-	// You fill in this part
+	public void run() {
+        fillRowWithBeepers();
+        pickEndBeepers();
+    }
 
+    /*
+     * Put a beeper on every corner of the St. except the first and last corners.
+     * Pre-condition: Be in home position (1st St. and 1st Ave.) facing east.
+     * Post-condition: Beepers are on each corner of 1st St. except the first and last corner.
+     *                 Karel is on the last corner of 1st St. facing east.
+     */
+    private void fillRowWithBeepers() {
+        move();
+        while(frontIsClear()) {
+            putBeeper();
+            move();
+        }
+    }
+
+    /*
+     * Recursively remove beepers from the end of each St. of beepers until Karel finds the
+     * midpoint of the St. Then put Beeper on midpoint.
+     */
+    private void pickEndBeepers() {
+        turnAround();
+        move();
+        if (beepersPresent()) {
+            pickBeeper();
+            move();
+            while (beepersPresent()) {
+                move();
+            }
+            pickEndBeepers();
+        } else {
+            putBeeper();
+        }
+    }
 }
