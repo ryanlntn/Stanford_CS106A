170e401af52c67450f2534ef2a9015ce620a87e2
Ryan Linton <ryanlntn@gmail.com>
2014-01-10 19:16:24 -0800

    Add section 1 problem

---
+++ src/Section/DemocracyDefendingKarel.java
@@ -0,0 +1,15 @@
+/*
+ * File: DemocracyDefendingKarel.java
+ * ----------------------------
+ * Democracy defending Karel removes hanging chads from ballots
+ * if the center of the rectangle doesn't have a beeper.
+ */
+
+import stanford.karel.*;
+
+public class DemocracyDefendingKarel extends SuperKarel {
+
+    public void run() {
+    }
+
+}
