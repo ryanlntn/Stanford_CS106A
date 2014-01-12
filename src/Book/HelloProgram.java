23232d745944ef7fd3fa0553679f4b6919565f0f
Ryan Linton <ryanlntn@gmail.com>
2014-01-12 15:51:25 -0800

    Add HelloProgram from book

---
+++ src/Book/HelloProgram.java
@@ -0,0 +1,17 @@
+/*
+ * File: HelloProgram.java
+ * -----------------------
+ * This program displays "hello, world" on the screen.
+ * It is inspired by the first program in Brian
+ * Kernighan and Dennis Ritchie's classic book,
+ * The C Programming Language.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class HelloProgram extends GraphicsProgram {
+   public void run() {
+      add(new GLabel("hello, world"), 100, 75);
+   }
+}
