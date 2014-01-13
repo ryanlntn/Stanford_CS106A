25757f5ee48e6fa336a42326e2652ffa37356d67
Ryan Linton <ryanlntn@gmail.com>
2014-01-12 16:26:36 -0800

    Add Add2Integers from book

---
+++ src/Book/Add2Integers.java
@@ -0,0 +1,17 @@
+/*
+ * File: Add2Integers.java
+ * -----------------------
+ * This program adds two integers and prints their sum.
+ */
+
+import acm.program.*;
+
+public class Add2Integers extends ConsoleProgram {
+    public void run() {
+        println("This program adds two integers.");
+        int n1 = readInt("Enter n1: ");
+        int n2 = readInt("Enter n2: ");
+        int total = n1 + n2;
+        println("The total is " + total + ".");
+    }
+}
