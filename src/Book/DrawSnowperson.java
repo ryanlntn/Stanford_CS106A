8117e33771d31dd4ac406d2cecdb84e7ce02da16
Ryan Linton <ryanlntn@gmail.com>
2014-01-15 08:14:16 -0800

    Add buttons to snowperson

--- src/Book/DrawSnowperson.java
+++ src/Book/DrawSnowperson.java
@@ -15,20 +15,29 @@ import acm.program.*;
 public class DrawSnowperson extends GraphicsProgram {
     public void run() {
         drawBody();
+        drawButtons();
         drawFace();
     }
 
     private void drawFace() {
         // Face starts at 350x100 and goes to 400x150
         add(new GOval(360, 120, 5, 5));
-        add(new GOval(390, 120, 5, 5));
-        add(new GOval(370, 130, 10, 10));
-        add(new GLabel("V"), 373, 120);
+        add(new GOval(385, 120, 5, 5));
+        add(new GOval(370, 135, 10, 10));
+        add(new GLabel("V"), 372, 134);
     }
 
     private void drawBody() {
         add(new GOval(350, 100, 50, 50));
         add(new GOval(325, 150, 100, 100));
         add(new GOval(300, 250, 150, 150));
+    }
+
+    private void drawButtons() {
+        int y = 175;
+        for (int i = 0; i < 3; i++) {
+            add(new GRect(370, y, 10, 10));
+            y += 20;
+        }
     }
 }
