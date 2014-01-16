24106cd73bdcd46c84778c89d67fc58bedbd83f2
Ryan Linton <ryanlntn@gmail.com>
2014-01-15 22:20:35 -0800

    Extend InchesToCentimeters from Exercise 1 in book

--- src/Book/CentimetersToFeetAndInches.java
+++ src/Book/CentimetersToFeetAndInches.java
@@ -10,7 +10,8 @@ import acm.program.*;
 public class CentimetersToFeetAndInches extends ConsoleProgram {
 
     public void run() {
-        println("This program converts centimeters to feet and inches."); double cm = readDouble("Enter value in centimeters: ");
+        println("This program converts centimeters to feet and inches.");
+        double cm = readDouble("Enter value in centimeters: ");
         double totalInches = cm / CENTIMETERS_PER_INCH;
         int feet = (int) (totalInches / INCHES_PER_FOOT);
         double inches = totalInches - INCHES_PER_FOOT * feet; println(cm + "cm = " + feet + "ft + " + inches + "in");
--- src/Book/InchesToCentimeters.java
+++ src/Book/InchesToCentimeters.java
@@ -9,10 +9,11 @@ import acm.program.*;
 public class InchesToCentimeters extends ConsoleProgram {
 
     public void run() {
-        println("This program converts inches to centimeters.");
-        double inches = readDouble("Enter value in inches: ");
-        double cm = inches * CENTIMETERS_PER_INCH;
-        println(inches + "in = " + cm + "cm");
+        println("This program converts feet and inches to centimeters.");
+        double feet = readDouble("Enter number of feet: ");
+        double inches = readDouble("Enter number of inches: ");
+        double cm = (feet * 12 + inches) * CENTIMETERS_PER_INCH;
+        println(feet + "ft " + inches + "in = " + cm + "cm");
     }
 
     private static final double CENTIMETERS_PER_INCH = 2.54;
