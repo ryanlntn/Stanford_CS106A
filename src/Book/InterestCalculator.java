f5be973f94e69ff395d22a2e103930bc7af18bc6
Ryan Linton <ryanlntn@gmail.com>
2014-01-16 07:08:54 -0800

    Add InterestCalculator from Exercise 2 in book

---
+++ src/Book/InterestCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * File: InterestCalculator.java
+ * ------------------------------
+ * This program converts inches to centimeters.
+ */
+
+import acm.program.*;
+
+public class InterestCalculator extends ConsoleProgram {
+
+    public void run() {
+        println("Interest calculation program");
+        double balance = readDouble("Enter starting balance: ");
+        double rate = readDouble("Enter annual interest rate: ");
+        balance *= (1 + (rate / 100));
+        println("Balance after one year = " + balance);
+    }
+
+}
