31118119e3f8c2a7867bf6baa6a488ea751461b5
Ryan Linton <ryanlntn@gmail.com>
2014-01-16 07:22:42 -0800

    Extend InterestCalculator as specified in Exercise 3

--- src/Book/InterestCalculator.java
+++ src/Book/InterestCalculator.java
@@ -13,7 +13,9 @@ public class InterestCalculator extends ConsoleProgram {
         double balance = readDouble("Enter starting balance: ");
         double rate = readDouble("Enter annual interest rate: ");
         balance *= (1 + (rate / 100));
-        println("Balance after one year = " + balance);
+        println("Balance after one year = " + Math.round(balance * 100.0) / 100.0);
+        balance *= (1 + (rate / 100));
+        println("Balance after two years = " + Math.round(balance * 100.0) / 100.0);
     }
 
 }
