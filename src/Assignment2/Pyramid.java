deae0c3ca1a2f8ce5ff30efaab953a360e64048a
Ryan Linton <ryanlntn@gmail.com>
2014-01-17 09:54:26 -0800

    Get the first brick in the right place for Assignment 2 Pyramid

--- src/Assignment2/Pyramid.java
+++ src/Assignment2/Pyramid.java
@@ -1,7 +1,7 @@
 /*
  * File: Pyramid.java
- * Name: 
- * Section Leader: 
+ * Name: Ryan Linton
+ * Section Leader: N/A
  * ------------------
  * This file is the starter file for the Pyramid problem.
  * It includes definitions of the constants that match the
@@ -26,7 +26,9 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		/* You fill this in. */
+        int x = (getWidth() - (BRICKS_IN_BASE * BRICK_WIDTH)) / 2;
+        int y = getHeight() - BRICK_HEIGHT;
+		add(new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT));
 	}
 }
 
