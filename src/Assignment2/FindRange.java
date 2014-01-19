4be11311a83f02eddef3e453722708458c88bfd5
Ryan Linton <ryanlntn@gmail.com>
2014-01-18 22:43:10 -0800

    Add sloppy solution to FindRange form Assignment 2

--- src/Assignment2/FindRange.java
+++ src/Assignment2/FindRange.java
@@ -1,16 +1,48 @@
 /*
  * File: FindRange.java
- * Name: 
+ * Name: Ryan Linton
  * Section Leader: 
  * --------------------
- * This file is the starter file for the FindRange problem.
+ * This program finds the largest and smallest numbers from
+ * those provided by the user. It reads numbers until the
+ * sentinel value is read.
  */
 
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
+
+    private static final int EXIT = 0;
+
 	public void run() {
-		/* You fill this in */
+
+        println("This program finds the largest and smallest numbers.");
+
+        int value = readInt("? ");
+
+        if (value != 0) {
+            int smallest = value;
+            int largest = smallest;
+            while (true) {
+                value = readInt("? ");
+                if (value == 0) {
+                    break;
+                }
+                if (value > largest) {
+                    largest = value;
+                }
+                if (value > 0 && value < smallest) {
+                    smallest = value;
+                }
+            }
+            println("smallest: " + smallest);
+            println("largest: " + largest);
+
+        } else {
+            println("You gotta give me something to work with here...");
+        }
+
 	}
+
 }
 
