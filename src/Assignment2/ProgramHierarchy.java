defe95768752dfed383536d1b50e0cb29a920891
Ryan Linton <ryanlntn@gmail.com>
2014-01-18 16:11:10 -0800

    Start working on ProgramHierarchy from Assignment 2

--- src/Assignment2/ProgramHierarchy.java
+++ src/Assignment2/ProgramHierarchy.java
@@ -1,16 +1,21 @@
 /*
  * File: ProgramHierarchy.java
- * Name: 
- * Section Leader: 
+ * Name: Ryan Linton
+ * Section Leader: N/A
  * ---------------------------
- * This file is the starter file for the ProgramHierarchy problem.
+ * This program draws a diagram of the GraphicsProgram class hierarchy.
  */
 
 import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class ProgramHierarchy extends GraphicsProgram {	
+public class ProgramHierarchy extends GraphicsProgram {
+
+    private static final int BOX_WIDTH = 60;
+
+    private static final int BOX_HEIGHT = 20;
+
 	public void run() {
 		/* You fill this in. */
 	}
