fd7cf4e0669f12973d598fcbd74c1285504ef5e7
Ryan Linton <ryanlntn@gmail.com>
2014-01-18 16:03:32 -0800

    Add comments

--- src/Assignment2/Target.java
+++ src/Assignment2/Target.java
@@ -18,10 +18,12 @@ public class Target extends GraphicsProgram {
     private static final int INNER_SIZE = (int) (OUTER_SIZE * 0.3);
 
     public void run() {
+        // Create initial concentric circles
 		GOval outer = new GOval((getWidth() - OUTER_SIZE) / 2, (getHeight() - OUTER_SIZE) / 2, OUTER_SIZE, OUTER_SIZE);
         GOval white = new GOval((getWidth() - WHITE_SIZE) / 2, (getHeight() - WHITE_SIZE) / 2, WHITE_SIZE, WHITE_SIZE);
         GOval inner = new GOval((getWidth() - INNER_SIZE) / 2, (getHeight() - INNER_SIZE) / 2, INNER_SIZE, INNER_SIZE);
 
+        // Color the circles
         outer.setColor(Color.RED);
         outer.setFilled(true);
         outer.setFillColor(Color.RED);
@@ -32,6 +34,7 @@ public class Target extends GraphicsProgram {
         inner.setFilled(true);
         inner.setFillColor(Color.RED);
 
+        // Add the circles to the canvas
         add(outer);
         add(white);
         add(inner);
