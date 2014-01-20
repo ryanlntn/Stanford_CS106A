292b0f02bba14f341327fd93cb747689d7a5600d
Ryan Linton <ryanlntn@gmail.com>
2014-01-19 20:44:31 -0800

    Solve the Hailstone program from Assignment 2

--- src/Assignment2/Hailstone.java
+++ src/Assignment2/Hailstone.java
@@ -1,16 +1,43 @@
 /*
  * File: Hailstone.java
- * Name: 
- * Section Leader: 
+ * Name: Ryan Linton
+ * Section Leader: N/A
  * --------------------
- * This file is the starter file for the Hailstone problem.
+ * This program computes the Hailstone sequence.
  */
 
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
+    
 	public void run() {
-		/* You fill this in */
+		/*
+		 * Pick some positive integer and call it n.
+         * If n is even, divide it by two.
+         * If n is odd, multiply it by three and add one.
+         * Continue this process until n is equal to one.
+         */
+
+        int n = readInt("Enter a number: ");
+        int t = 0;
+
+        while (n != 1) {
+
+            int old = n;
+
+            if (n % 2 == 0) {
+                n /= 2;
+                println(old + " is even so I take half: " + n);
+            } else {
+                n = n * 3 + 1;
+                println(old + " is odd, so I make 3n + 1: " + n);
+            }
+
+            t++;
+        }
+
+        println("The process took " + t + " to reach 1");
+
 	}
 }
 
