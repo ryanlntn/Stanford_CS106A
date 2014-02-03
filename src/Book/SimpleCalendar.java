4c2a49ffd8483f70a475ddc82621911b177c2f87
Ryan Linton <ryanlntn@gmail.com>
2014-02-03 08:45:46 -0800

    Fix SimpleCalendar to print correct number of rows

--- src/Book/SimpleCalendar.java
+++ src/Book/SimpleCalendar.java
@@ -12,7 +12,7 @@ import acm.graphics.*;
 public class SimpleCalendar extends GraphicsProgram {
 
     /* The number of days in the month */
-    private static final int DAYS_IN_MONTH = 31;
+    private static final int DAYS_IN_MONTH = 28;
 
     /* The day of the week on which the month starts */
     /* (Sunday = 0, Monday = 1, Tuesday = 2, and so on) */
@@ -26,11 +26,11 @@ public class SimpleCalendar extends GraphicsProgram {
 
     public void run() {
         int current_day = 0;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < DAYS_IN_MONTH / 5; i++) {
             for (int j = 0; j < 7; j++) {
                 add(new GRect(j * DAY_WIDTH, i * DAY_HEIGHT, DAY_WIDTH, DAY_HEIGHT));
                 if (i == 0 && j == DAY_MONTH_STARTS) current_day = 1;
-                if (current_day > 0 && current_day < DAYS_IN_MONTH) {
+                if (current_day > 0 && current_day <= DAYS_IN_MONTH) {
                     add(new GLabel("" + current_day, j * DAY_WIDTH + (DAY_WIDTH / 8), i * DAY_HEIGHT + (DAY_HEIGHT / 2)));
                     current_day++;
                 }
