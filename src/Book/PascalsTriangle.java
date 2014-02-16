cdbf585ca8de4aaf098e59e7fe00de118e7d7a93
Ryan Linton <ryanlntn@gmail.com>
2014-02-16 09:41:26 -0800

    Add PascalsTriangle from the book

---
+++ src/Book/PascalsTriangle.java
@@ -0,0 +1,17 @@
+/*
+ * File: PascalsTriangle.java
+ * -----------------------
+ * This program displays Pascal's Triangle.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class PascalsTriangle extends GraphicsProgram {
+
+    public void run() {
+        add(new GLabel("I love Java!"), 100, 75);
+        add(new GLabel("- Ryan Linton"), 600, 400);
+    }
+
+}
