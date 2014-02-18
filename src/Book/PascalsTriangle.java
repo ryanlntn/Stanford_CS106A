75e656b28980917b9f63b6581139706a9e58ae1a
Ryan Linton <ryanlntn@gmail.com>
2014-02-17 21:38:39 -0800

    Display as shown in book

--- src/Book/PascalsTriangle.java
+++ src/Book/PascalsTriangle.java
@@ -12,7 +12,7 @@ public class PascalsTriangle extends GraphicsProgram {
     public void run() {
         for (int i = 0; i <= 8; i++) {
             for (int j = 0; j <= i; j++) {
-                add(new GLabel("C(" + i + "," + j + ")"), j * 40 + 100, i * 20 + 100);
+                add(new GLabel("C(" + i + "," + j + ")"), 40 * j + getWidth() / 2 - 20 * i, i * 20 + 100);
             }
         }
 
