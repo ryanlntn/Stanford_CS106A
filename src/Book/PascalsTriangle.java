128240edd12ca68828b186fc4a4dd35089f7c648
Ryan Linton <ryanlntn@gmail.com>
2014-02-17 21:34:06 -0800

    Calculate actual PascalsTriangle values

--- src/Book/PascalsTriangle.java
+++ src/Book/PascalsTriangle.java
@@ -10,8 +10,12 @@ import acm.program.*;
 public class PascalsTriangle extends GraphicsProgram {
 
     public void run() {
-        add(new GLabel("I love Java!"), 100, 75);
-        add(new GLabel("- Ryan Linton"), 600, 400);
+        for (int i = 0; i <= 8; i++) {
+            for (int j = 0; j <= i; j++) {
+                add(new GLabel("C(" + i + "," + j + ")"), j * 40 + 100, i * 20 + 100);
+            }
+        }
+
     }
 
 }
