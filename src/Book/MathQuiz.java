47d3af2bce1f32343893302c7cf71331ee962516
Ryan Linton <ryanlntn@gmail.com>
2014-03-01 12:24:41 -0800

    Ugly solution to MathQuiz exericise

--- src/Book/MathQuiz.java
+++ src/Book/MathQuiz.java
@@ -6,10 +6,47 @@ import acm.util.*;
  */
 public class MathQuiz extends ConsoleProgram {
 
-    private static final int MAX
+    private static final int TOTAL_QUESTIONS = 5;
+    private static final int MAX_ATTEMPTS = 3;
 
     public void run() {
-
+        for (int i = 0; i < TOTAL_QUESTIONS; i++) {
+            askQuestion();
+        }
     }
+
+    private void askQuestion() {
+        int x = rand.nextInt(0, 20);
+        int y = rand.nextInt(0, 20);
+        int attempts = 0;
+
+        if (rand.nextBoolean()) {
+            int ans = readInt("What is " + x + " + " + y + "? ");
+            while (ans != x + y) {
+                attempts++;
+                if (attempts == MAX_ATTEMPTS) {
+                    println("No, the answer is " + (x + y) + ".");
+                    return;
+                }
+                println("That's incorrect - try a different answer: ");
+                ans = readInt("What is " + x + " + " + y + "? ");
+            }
+            println("That's the answer!");
+        } else {
+            int ans = readInt("What is " + x + " - " + y + "? ");
+            while (ans != x - y) {
+                attempts++;
+                if (attempts == MAX_ATTEMPTS) {
+                    println("No, the answer is " + (x - y) + ".");
+                    return;
+                }
+                println("That's incorrect - try a different answer: ");
+                ans = readInt("What is " + x + " - " + y + "? ");
+            }
+            println("That's the answer!");
+        }
+    }
+
+    private RandomGenerator rand = new RandomGenerator();
 
 }
