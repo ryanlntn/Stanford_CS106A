7fbbe2733e37e26e17312f22e2d8efaa3d9bd0d3
Ryan Linton <ryanlntn@gmail.com>
2014-03-01 11:51:24 -0800

    Add RadioactiveDecay simulator from exercises

---
+++ src/Book/RadioactiveDecay.java
@@ -0,0 +1,25 @@
+import acm.program.*;
+import acm.util.*;
+
+/**
+ * This program simulates radioactive decay.
+ */
+public class RadioactiveDecay extends ConsoleProgram {
+
+    private static final int initialAtoms = 10000;
+
+    public void run() {
+        println("There are " + initialAtoms + " atoms initially");
+        int atoms = initialAtoms;
+        int year = 0;
+        while (atoms > 0) {
+            atoms *= rand.nextDouble(0.45, 0.55);
+            year++;
+            println("There are " + atoms + " atoms at the end of year " + year);
+        }
+    }
+
+    /* Create an instance variable for the random number generator */
+    private RandomGenerator rand = new RandomGenerator();
+
+}
