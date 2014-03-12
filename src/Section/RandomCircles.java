e0cbcf42fee02c7a82922f196391fd0087afd147
Ryan Linton <ryanlntn@gmail.com>
2014-03-12 08:19:01 -0700

    Add BouncingBall and RandomCircles from Section Handout

---
+++ src/Section/RandomCircles.java
@@ -0,0 +1,38 @@
+/*
+ * File: BouncingBall.java
+ * -----------------------
+ * This program graphically simulates a bouncing ball.
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+
+import java.awt.*;
+
+public class RandomCircles extends GraphicsProgram {
+
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+
+            int radius = rand.nextInt(5, 100);
+            int x = rand.nextInt(0, getWidth() - radius * 2);
+            int y = rand.nextInt(0, getHeight() - radius * 2);
+            Color fillColor = rand.nextColor();
+
+            GOval circle = new GOval(x, y, radius, radius);
+            circle.setFilled(true);
+            circle.setFillColor(fillColor);
+            circle.setColor(fillColor);
+
+            add(circle);
+
+        }
+
+    }
+
+    /* Create an instance variable for the random number generator */
+    private RandomGenerator rand = new RandomGenerator();
+
+}
