177bd0a09ba3f68de00c694c0e421e5b36a20e82
Ryan Linton <ryanlntn@gmail.com>
2014-03-14 08:39:52 -0700

    Add DrawRectangle from the book :

---
+++ src/Book/DrawRectangle.java
@@ -0,0 +1,43 @@
+/*
+ * File: DrawRectangle.java
+ * ------------------------
+ * This program allows users to create rectangles on the canvas
+ * by clicking and dragging with the mouse.
+ */
+
+import java.awt.event.*;
+import acm.graphics.*;
+import acm.program.*;
+
+/** This class allows users to drag rectangles on the canvas */
+public class DrawRectangle extends GraphicsProgram {
+
+    /** Runs the program */
+    public void run() {
+        addMouseListeners();
+    }
+
+    /** Called on mouse press to record the starting coordinates */
+    public void mousePressed(MouseEvent e) {
+        startX = e.getX();
+        startY = e.getY();
+        currentRect = new GRect(startX, startY, 0, 0);
+        currentRect.setFilled(true);
+        add(currentRect);
+    }
+
+    /** Called on mouse drag to reshape the current rectangle */
+    public void mouseDragged(MouseEvent e) {
+        double x = Math.min(e.getX(), startX);
+        double y = Math.min(e.getY(), startY);
+        double width = Math.abs(e.getX() - startX);
+        double height = Math.abs(e.getY() - startY);
+        currentRect.setBounds(x, y, width, height);
+    }
+
+    /* Private state */
+    private GRect currentRect; /* The current rectangle */
+    private double startX; /* The initial mouse X position  */
+    private double startY; /* The initial mouse Y position  */
+
+}
