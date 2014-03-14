d1a5c92ec96012a3111fcc6e798c456b947f70f1
Ryan Linton <ryanlntn@gmail.com>
2014-03-13 19:47:00 -0700

    Unable to compile draw lines. No clue why

--- src/Section/DrawLines.java
+++ src/Section/DrawLines.java
@@ -4,14 +4,27 @@
  * This program allows a user to draw lines to the canvas.
  */
 
-import acm.program.*;
+import java.awt.event.*;
 import acm.graphics.*;
-import acm.util.*;
+import acm.program.*;
 
-public class RandomCircles extends GraphicsProgram {
+public class DrawLines extends GraphicsProgram {
 
     public void run() {
-        
+        addMouseListeners();
     }
+
+    /** Called on mouse press to record the coordinates of the click */
+    public void mousePressed(MouseEvent e) {
+        line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+        add(line);
+    }
+
+    /** Called on mouse drag to reposition the object */
+    public void mouseDragged(MouseEvent e) {
+        if (line != null) line.setEndPoint(e.getX(), e.getY());
+    }
+
+    private GLine line;
 
 }
