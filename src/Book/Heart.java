6fab7aed47e86670c174ee543e7537df1e8d3f37
Ryan Linton <ryanlntn@gmail.com>
2014-03-27 21:42:38 -0700

    Finally starting to resemble a heart

--- src/Book/Heart.java
+++ src/Book/Heart.java
@@ -5,10 +5,10 @@ import acm.program.GraphicsProgram;
 public class Heart extends GraphicsProgram {
 
     public void run() {
-        add(new GArc((getWidth() / 2) - 100, 100, 100, 100, 45, 180));
-        add(new GArc((getWidth() / 2), 100, 100, 100, 315, 180));
-        add(new GLine((getWidth() / 2) - 100, 200, getWidth() / 2, 300));
-        add(new GLine((getWidth() / 2) + 100, 200, getWidth() / 2, 300));
+        add(new GArc((getWidth() / 2) - (100 / Math.sqrt(2)) - 15, 115, 100, 100, 45, 180));
+        add(new GArc((getWidth() / 2) - 15, 115, 100, 100, 315, 180));
+        add(new GLine((getWidth() / 2) - (100 / Math.sqrt(2)), 200, getWidth() / 2, 200 + (100 / Math.sqrt(2))));
+        add(new GLine((getWidth() / 2) + (100 / Math.sqrt(2)), 200, getWidth() / 2, 200 + (100 / Math.sqrt(2))));
     }
 
 }
