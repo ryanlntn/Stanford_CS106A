240fe56d8e94147957d1a467b694f9cb23434fd4
Ryan Linton <ryanlntn@gmail.com>
2014-03-31 22:55:25 -0700

    Give Pumpkin body and eyes

--- src/Book/HalloweenPumpkin.java
+++ src/Book/HalloweenPumpkin.java
@@ -1,19 +1,47 @@
 import java.awt.Color;
-
-import acm.graphics.GRect;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class HalloweenPumpkin extends GraphicsProgram {
 
     public void run() {
         drawStem();
+        drawBody();
+        GPoint[] leftEyePoints = {
+            new GPoint((getWidth() / 2) - 20, 50),
+            new GPoint((getWidth() / 2) - 25, 55),
+            new GPoint((getWidth() / 2) - 15, 55)
+        };
+        GPoint[] rightEyePoints = {
+                new GPoint((getWidth() / 2) + 20, 50),
+                new GPoint((getWidth() / 2) + 25, 55),
+                new GPoint((getWidth() / 2) + 15, 55)
+        };
+        drawEye(leftEyePoints);
+        drawEye(rightEyePoints);
+
     }
 
     private void drawStem() {
-        GRect stem = new GRect(getWidth() / 2 - 2, 20, getWidth() / 2 + 2, 25);
+        GRect stem = new GRect((getWidth() / 2) - 2, 20, 4, 5);
         stem.setFilled(true);
         stem.setFillColor(Color.BLACK);
         add(stem);
+    }
+
+    private void drawBody() {
+        GOval body = new GOval((getWidth() / 2) - 50, 25, 100, 100);
+        body.setFilled(true);
+        body.setFillColor(Color.ORANGE);
+        body.setColor(Color.ORANGE);
+        add(body);
+    }
+
+    private void drawEye(GPoint[] points) {
+        GPolygon eye = new GPolygon(points);
+        eye.setFilled(true);
+        eye.setFillColor(Color.BLACK);
+        add(eye);
     }
 
 }
