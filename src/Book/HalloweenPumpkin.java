1d6021e48fbfc35e186498e2f6df216e3da23b25
Ryan Linton <ryanlntn@gmail.com>
2014-03-31 23:06:32 -0700

    Add mouth half implemented; probably keep it this way because it looks cool

--- src/Book/HalloweenPumpkin.java
+++ src/Book/HalloweenPumpkin.java
@@ -8,18 +8,24 @@ public class HalloweenPumpkin extends GraphicsProgram {
         drawStem();
         drawBody();
         GPoint[] leftEyePoints = {
-            new GPoint((getWidth() / 2) - 20, 50),
-            new GPoint((getWidth() / 2) - 25, 55),
-            new GPoint((getWidth() / 2) - 15, 55)
+            new GPoint((getWidth() / 2) - 20, 55),
+            new GPoint((getWidth() / 2) - 25, 50),
+            new GPoint((getWidth() / 2) - 15, 50)
         };
         GPoint[] rightEyePoints = {
-                new GPoint((getWidth() / 2) + 20, 50),
-                new GPoint((getWidth() / 2) + 25, 55),
-                new GPoint((getWidth() / 2) + 15, 55)
+            new GPoint((getWidth() / 2) + 20, 55),
+            new GPoint((getWidth() / 2) + 25, 50),
+            new GPoint((getWidth() / 2) + 15, 50)
         };
-        drawEye(leftEyePoints);
-        drawEye(rightEyePoints);
-
+        GPoint[] nosePoints = {
+            new GPoint((getWidth() / 2), 60),
+            new GPoint((getWidth() / 2) + 5, 65),
+            new GPoint((getWidth() / 2) - 5, 65)
+        };
+        drawTriangle(leftEyePoints);
+        drawTriangle(rightEyePoints);
+        drawTriangle(nosePoints);
+        drawMouth();
     }
 
     private void drawStem() {
@@ -37,11 +43,31 @@ public class HalloweenPumpkin extends GraphicsProgram {
         add(body);
     }
 
-    private void drawEye(GPoint[] points) {
-        GPolygon eye = new GPolygon(points);
-        eye.setFilled(true);
-        eye.setFillColor(Color.BLACK);
-        add(eye);
+    private void drawTriangle(GPoint[] points) {
+        GPolygon triangle = new GPolygon(points);
+        triangle.setFilled(true);
+        triangle.setFillColor(Color.BLACK);
+        add(triangle);
+    }
+
+    private void drawMouth() {
+        GPoint[] points = {
+            new GPoint((getWidth() / 2) - 25, 80),
+            new GPoint((getWidth() / 2) - 20, 75),
+            new GPoint((getWidth() / 2) - 15, 80),
+            new GPoint((getWidth() / 2) - 10, 75),
+            new GPoint((getWidth() / 2) - 5, 80),
+            new GPoint((getWidth() / 2), 75),
+            new GPoint((getWidth() / 2) + 25, 80),
+            new GPoint((getWidth() / 2) + 20, 90),
+            new GPoint((getWidth() / 2) + 15, 85),
+            new GPoint((getWidth() / 2) + 10, 90),
+            new GPoint((getWidth() / 2) + 5, 85),
+        };
+        GPolygon mouth = new GPolygon(points);
+        mouth.setFilled(true);
+        mouth.setFillColor(Color.BLACK);
+        add(mouth);
     }
 
 }
