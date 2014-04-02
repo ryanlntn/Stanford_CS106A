c97341953282f55d05c02184f549146752ae9e24
Ryan Linton <ryanlntn@gmail.com>
2014-04-02 08:17:24 -0700

    Add HouseOfUsher from the book

--- src/Book/HouseOfUsher.java
+++ src/Book/HouseOfUsher.java
@@ -2,35 +2,37 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
-public class House extends GraphicsProgram {
+public class HouseOfUsher extends GraphicsProgram {
 
     public void run() {
-        add(new GRect(100, 100, 600, 400));
-        add(new GLine(100, 100, 400, 50));
-        add(new GLine(400, 50, 700, 100));
+        add(new GRect(150, 100, 550, 400));
+        add(new GLine(150, 100, 450, 50));
+        add(new GLine(450, 50, 700, 100));
         drawSmallWindow(225, 200);
         drawSmallWindow(525, 200);
         drawLargeWindow(200, 400);
         drawLargeWindow(525, 400);
         drawDoor(375, 400);
+        drawTower(100, 50, 50, 450);
+        drawTower(700, 50, 50, 450);
     }
 
     private void drawSmallWindow(double x, double y) {
         add(new GRect(x, y, 50, 50));
-        add(new GLine(x + 25, y, x + 25, y + 50));
-        add(new GLine(x, y + 25, x + 50, y + 25));
     }
 
     private void drawLargeWindow(double x, double y) {
         add(new GRect(x, y, 75, 50));
-        add(new GLine(x, y + 25, x + 75, y + 25));
-        add(new GLine(x + 25, y, x + 25, y + 50));
-        add(new GLine(x + 50, y, x + 50, y + 50));
     }
 
     private void drawDoor(double x, double y) {
         add(new GRect(x, y, 50, 100));
-        add(new GOval(x + 50 - 10, y + 52, 5, 5));
+    }
+
+    private void drawTower(double x, double y, double w, double h) {
+        add(new GRect(x, y + 50, w, h - 50));
+        add(new GLine(x, y + 50, x + w / 2, y));
+        add(new GLine(x + w / 2, y, x + w, y + 50));
     }
 
 }
