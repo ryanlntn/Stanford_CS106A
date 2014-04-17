bf30f5ccbf815aa050846fef37bf92114ea23187
Ryan Linton <ryanlntn@gmail.com>
2014-04-16 23:53:12 -0700

    Get started on Hangman

--- src/Assignment4/Hangman.java
+++ src/Assignment4/Hangman.java
@@ -13,8 +13,29 @@ import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
 
+    private HangmanLexicon lexicon;
+    private String word;
+    private int remainingGuesses = 8;
+
     public void run() {
 		/* You fill this in */
+        lexicon = new HangmanLexicon();
+        word = lexicon.getWord(0);
+        println("Welcome to Hangman!");
+        println("The word now looks like this: " + obfuscatedWord());
+        println("You have " + remainingGuesses + " guesses left.");
+        String guess = readLine("Your guess: ").toUpperCase();
+        println(guess);
 	}
+
+    private String obfuscatedWord() {
+        String dashedWord = "";
+        for (int i = 0; i < word.length(); i++) {
+            dashedWord += "-";
+        }
+        return dashedWord;
+    }
+
+
 
 }
