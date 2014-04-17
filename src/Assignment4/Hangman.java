0202ff226dec77058df7eb6dda3091edc4178482
Ryan Linton <ryanlntn@gmail.com>
2014-04-17 10:29:11 -0700

    Move guess handling into it's own method

--- src/Assignment4/Hangman.java
+++ src/Assignment4/Hangman.java
@@ -14,31 +14,53 @@ import java.awt.*;
 public class Hangman extends ConsoleProgram {
 
     private HangmanLexicon lexicon;
-    private String word;
+    private String actualWord;
+    private String guessedWord;
     private int remainingGuesses = 8;
     private RandomGenerator rand = RandomGenerator.getInstance();
 
     public void run() {
         lexicon = new HangmanLexicon();
-        word = lexicon.getWord(rand.nextInt(0, lexicon.getWordCount()));
+        actualWord = lexicon.getWord(rand.nextInt(0, lexicon.getWordCount() - 1));
+        guessedWord = hideWord(actualWord);
         println("Welcome to Hangman!");
         while (true) {
-            if (remainingGuesses == 0) break;
-            println("The word now looks like this: " + obfuscatedWord());
+            if (remainingGuesses == 0 || isWordGuessed()) break;
+
+            println("The word now looks like this: " + guessedWord);
             println("You have " + remainingGuesses + " guesses left.");
+
             String guess = readLine("Your guess: ").toUpperCase();
             println("Your guess: " + guess);
+            takeGuess(guess);
             println("There are no " + guess + "'s in the word.");
+
             remainingGuesses--;
         }
 	}
 
-    private String obfuscatedWord() {
+    private String hideWord(String originalWord) {
         String dashedWord = "";
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < originalWord.length(); i++) {
             dashedWord += "-";
         }
         return dashedWord;
+    }
+
+    private boolean isWordGuessed() {
+        return actualWord.equals(guessedWord);
+    }
+
+    private void takeGuess(String guess) {
+        String newGuessedWord = "";
+        for (int i = 0; i < actualWord.length(); i++) {
+            if (actualWord.substring(i, i + 1).equals(guess)) {
+                newGuessedWord += guess;
+            } else {
+                newGuessedWord += "-";
+            }
+        }
+        guessedWord = newGuessedWord;
     }
 
 
