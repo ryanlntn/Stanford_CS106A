1eeb5eb824c839fbd994b23efd64ae6d5323b536
Ryan Linton <ryanlntn@gmail.com>
2014-04-21 06:49:20 -0700

    Add canvas

--- src/Assignment4/Hangman.java
+++ src/Assignment4/Hangman.java
@@ -14,6 +14,7 @@ import java.awt.*;
 public class Hangman extends ConsoleProgram {
 
     private HangmanLexicon lexicon;
+    private HangmanCanvas canvas;
     private String actualWord;
     private String guessedWord;
     private int remainingGuesses = 8;
@@ -43,6 +44,11 @@ public class Hangman extends ConsoleProgram {
 
         closingMessage();
 	}
+
+    public void init() {
+        canvas = new HangmanCanvas();
+        add(canvas);
+    }
 
     private void closingMessage() {
         if (isWordGuessed()) {
