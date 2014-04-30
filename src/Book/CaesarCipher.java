2dc9b353aa1814f2fc8961b895e37685aace5939
Ryan Linton <ryanlntn@gmail.com>
2014-04-30 10:04:19 -0700

    Add CaesarCipher working in one direction

---
+++ src/Book/CaesarCipher.java
@@ -0,0 +1,26 @@
+import acm.program.ConsoleProgram;
+
+/**
+ * This program encodes a message using a Caeser cipher.
+ */
+public class CaesarCipher extends ConsoleProgram {
+
+    public void run() {
+        println("This program encodes a message using a Caeser cipher.");
+        while (true) {
+            int shift = readInt("Enter the number of character positions to shift: ");
+            if (shift == 0) break;
+            String message = readLine("Enter a message: ");
+            println("Encoded message: " + encodeString(message, shift));
+        }
+    }
+
+    private String encodeString(String message, int shift) {
+        String encodedMessage = "";
+        for (int i = 0; i < message.length(); i++) {
+            encodedMessage += (char)((message.charAt(i) + shift - (int)'a') % 26 + (int)'a');
+        }
+        return encodedMessage;
+    }
+
+}
