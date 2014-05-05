17555af0bd1bb142a5f6ae75550f9f6dbbc283c5
Ryan Linton <ryanlntn@gmail.com>
2014-05-04 22:49:06 -0700

    Extend is palindrome to accept sentences with punctuation

--- src/Book/Palindrome.java
+++ src/Book/Palindrome.java
@@ -13,12 +13,16 @@ public class Palindrome extends ConsoleProgram {
         while (true) {
             String str = readLine("Enter any string: ");
             if (str.length() == 0) break;
-            println(str + (isPalindrome(str) ? " is a planidrome." : " isn't a planidrome."));
+            println(str + (isSentencePalindrome(str) ? " is a planidrome." : " isn't a planidrome."));
         }
     }
 
     private boolean isPalindrome(String str) {
-      return str.equals(reverseString(str));
+        return str.equals(reverseString(str));
+    }
+
+    private boolean isSentencePalindrome(String str) {
+        return cleanString(str).equals(reverseString(cleanString(str)));
     }
 
     private String reverseString(String str) {
@@ -27,6 +31,15 @@ public class Palindrome extends ConsoleProgram {
           reversed = str.charAt(i) + reversed;
         }
         return reversed;
+    }
+
+    private String cleanString(String str) {
+        str = str.toLowerCase();
+        String clean = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) > 96 && str.charAt(i) < 123) clean += str.charAt(i);
+        }
+        return clean;
     }
 
 }
