4aae3b33a9341de37aa0866283bb626d8711bb3f
Ryan Linton <ryanlntn@gmail.com>
2014-05-27 21:22:17 -0700

    Clean up indentation

--- src/Assignment7/FacePamphlet.java
+++ src/Assignment7/FacePamphlet.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * File: FacePamphlet.java
  * -----------------------
  * When it is finished, this program will implement a basic social network
@@ -11,26 +11,25 @@ import acm.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class FacePamphlet extends Program 
-					implements FacePamphletConstants {
+public class FacePamphlet extends Program implements FacePamphletConstants {
 
-	/**
-	 * This method has the responsibility for initializing the 
-	 * interactors in the application, and taking care of any other 
-	 * initialization that needs to be performed.
-	 */
-	public void init() {
-		// You fill this in
+    /**
+     * This method has the responsibility for initializing the
+     * interactors in the application, and taking care of any other
+     * initialization that needs to be performed.
+     */
+    public void init() {
+        // You fill this in
     }
-    
-  
+
+
     /**
      * This class is responsible for detecting when the buttons are
      * clicked or interactors are used, so you will have to add code
      * to respond to these actions.
      */
     public void actionPerformed(ActionEvent e) {
-		// You fill this in as well as add any additional methods
-	}
+        // You fill this in as well as add any additional methods
+    }
 
 }
